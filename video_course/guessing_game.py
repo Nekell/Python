65b786c182bac2d542c0337bf54f259274dd99d9
Nekell <47759301+Nekell@users.noreply.github.com>
2021-01-22 21:01:05 +0300

    Update guessing_game.py
    Add local multiplayer

--- video_course/guessing_game.py
+++ video_course/guessing_game.py
@@ -1,22 +1,36 @@
 import random
 
-number = random.randit(1, 100)
-user_number = None
+number = random.randint(1, 100)
+#print(number)
+player_number = None
 count = 0
 levels = {1: 10, 2: 5, 3: 3}
 level = int(input("Выберите уровнь сложности 1-3: "))
 max_count = levels[level]
+player_count = int(input("Введите количество игроков: "))
+players = []
+for i in range(player_count):
+    player_nik = input(f"Введите имя {i+1} игрока: ")
+    players.append(player_nik)
+is_winner = False
+winner_name = None
 
-while user_number != number:
+while not is_winner:
     count += 1
-    if count > max_count
+    if count > max_count:
         print("Вы проиграли")
         break
     print(f"Попытка №{count}")
-    user_number = int(input("Угадайте число от 1 до 100: "))
-    if user_number < number:
-        print("Ваше число меньше загаданного")
-    elif user_number > number:
-        print("Ваше число больше загаданного")
+    for player in players:
+        print(f"Ход игрока: {player}")
+        player_number = int(input("Угадайте число от 1 до 100: "))
+        if player_number == number:
+            is_winner = True
+            winner_name = player
+            break
+        elif player_number < number:
+            print("Ваше число меньше загаданного")
+        else:
+            print("Ваше число больше загаданного")
 else:
-    print("Победа!")
+    print(f"Победил {player}!")
