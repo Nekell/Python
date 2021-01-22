471ab4c76d916c62ca50f6b9231fa918d388a9e4
Nekell <47759301+Nekell@users.noreply.github.com>
2021-01-22 15:49:37 +0300

    Update guessing_game.py
    Added counter and levels

--- video_course/guessing_game.py
+++ video_course/guessing_game.py
@@ -1,10 +1,22 @@
 import random
+
 number = random.randit(1, 100)
 user_number = None
-while user_number =! number:
+count = 0
+levels = {1: 10, 2: 5, 3: 3}
+level = int(input("Выберите уровнь сложности 1-3: "))
+max_count = levels[level]
+
+while user_number != number:
+    count += 1
+    if count > max_count
+        print("Вы проиграли")
+        break
+    print(f"Попытка №{count}")
     user_number = int(input("Угадайте число от 1 до 100: "))
     if user_number < number:
         print("Ваше число меньше загаданного")
     elif user_number > number:
         print("Ваше число больше загаданного")
-print("Верно!")
+else:
+    print("Победа!")
