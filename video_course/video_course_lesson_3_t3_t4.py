4ed5d9928303a1c9154087879374a96425ce6ef5
Nekell <47759301+Nekell@users.noreply.github.com>
2021-01-27 00:00:52 +0300

    Update video_course_lesson_3_t3_t4.py

--- video_course/video_course_lesson_3_t3_t4.py
+++ video_course/video_course_lesson_3_t3_t4.py
@@ -1,17 +1,15 @@
 player = {"name": input("Введите имя игрока: "), "health": 100, "damage": 20, "armor": 1.2}
-enemy = {"name": input("Введите имя противника: "), "health": 100, "damage": 20, "armor": 1.2}
+enemy = {"name": input("Введите имя противника: "), "health": 50, "damage": 10, "armor": 1.2}
 
 
 def attack(attacker, defender):
     defender["health"] -= real_damage(attacker["damage"], defender["armor"])
-    print(defender["health"])
 
 
 def real_damage(damage, armor):
-    result = damage / armor
-    print(result)
-    return result
+    return damage / armor
 
 
 attack(player, enemy)
-print(player, enemy)
+attack(enemy, player)
+print("Игрок атаковал противника, противник атаковал игрока. \nРезультат боя: ", player, enemy)
