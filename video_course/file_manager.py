ffc4b6927825a99c151023027d948c21d7acffd7
Nekell <47759301+Nekell@users.noreply.github.com>
2021-02-01 17:09:57 +0300

    Update file_manager.py

--- video_course/file_manager.py
+++ video_course/file_manager.py
@@ -1,4 +1,6 @@
 import os
+import shutil
+import datetime
 
 
 def create_file(name, text=None):
@@ -21,9 +23,38 @@ def get_list(folders_only=False):
     print(result)
 
 
+def delete_file(name):
+    if os.path.isdir(name):
+        os.rmdir(name)
+    else:
+        os.remove(name)
+
+
+def copy_file(name, new_name):
+    if os.path.isdir(name):
+        try:
+            shutil.copytree(name, new_name)
+        except FileExistsError:
+            print("Folder already exist!")
+    else:
+        shutil.copy(name, new_name)
+
+
+def save_info(message):
+    current_time = datetime.datetime.now()
+    result = f"{current_time} - {message}"
+    with open("log.txt", "a", encoding="utf-8") as f:
+        f.write(result + "\n")
+
+
 if __name__ == "__main__":
     create_file("text.dat")
     create_file("text.dat", "some_text")
     create_folder("new_folder")
     get_list()
     get_list(True)
+    delete_file("new_folder")
+    copy_file("new_folder", "new_folder2")
+    create_file("text.dat")
+    copy_file("text.dat", "text2.dat")
+    save_info("test message")
