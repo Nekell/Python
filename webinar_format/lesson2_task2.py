6bc69124c80701008976d3ce904654f939e5c5b2
Dmitry <ageev.dmitry.s@gmail.com>
2021-02-05 17:58:18 +0300

    rework l2 t2

---
+++ webinar_format/lesson2_task2.py
@@ -0,0 +1,10 @@
+"""
+2. Для списка реализовать обмен значений соседних элементов,
+т.е. значениями обмениваются элементы с индексами 0 и 1, 2 и 3 и т.д.
+При нечетном количестве элементов последний сохранить на своем месте.
+Для заполнения списка элементов необходимо использовать функцию input().
+"""
+another_list = input('Enter a number of elements separated by a space: ').split()
+for i in range(0, len(another_list) - 1, 2):
+    another_list[i], another_list[i+1] = another_list[i+1], another_list[i]
+print(another_list)
