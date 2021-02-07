454723b6bcf2533182b02af1a125087d64ed98ac
Dmitry <ageev.dmitry.s@gmail.com>
2021-02-07 16:19:54 +0300

    rework l2 t2 again

--- webinar_format/lesson2_task2.py
+++ webinar_format/lesson2_task2.py
@@ -6,5 +6,5 @@
 """
 another_list = input('Enter a number of elements separated by a space: ').split()
 for i in range(0, len(another_list) - 1, 2):
-    another_list[i], another_list[i+1] = another_list[i+1], another_list[i]
+    another_list[i], another_list[i + 1] = another_list[i + 1], another_list[i]
 print(another_list)
