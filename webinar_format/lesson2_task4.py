fb4aa1c740f9f929781cbf7cd909f5243f81fb41
Dmitry <ageev.dmitry.s@gmail.com>
2021-02-08 17:27:16 +0300

    add another solution

--- webinar_format/lesson2_task4.py
+++ webinar_format/lesson2_task4.py
@@ -5,5 +5,9 @@
 Если в слово длинное, выводить только первые 10 букв в слове.
 """
 and_another_list = input('Enter words separated by space: ').split()
-for i, word in enumerate(and_another_list):
-    print(f'{i + 1}: {word[:10]}')
+
+# for i, word in enumerate(and_another_list):
+#     print(f'{i + 1}: {word[:10]}')
+
+for i, word in enumerate(and_another_list, 1):
+    print(f'{i}: {word[:10]}')
