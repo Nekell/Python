f461a6f711041f578a8d30983a5d3f991b6a9dfe
Dmitry <ageev.dmitry.s@gmail.com>
2021-03-12 00:02:26 +0300

    rework l4 t4

---
+++ webinar_format/lesson4_task4.py
@@ -0,0 +1,22 @@
+"""
+4. Представлен список чисел.
+Определить элементы списка, не имеющие повторений.
+Сформировать итоговый массив чисел, соответствующих требованию.
+Элементы вывести в порядке их следования в исходном списке.
+Для выполнения задания обязательно использовать генератор.
+Пример исходного списка: [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11].
+Результат: [23, 1, 3, 10, 4, 11]
+"""
+
+
+def no_repetitions(some_list):
+    numbers_counter = {number: some_list.count(number) for number in some_list}
+    for number in some_list:
+        if numbers_counter[number] == 1:
+            yield number
+
+
+input_list = list(map(int, input('Введите числа для списка через пробел: ').split()))
+print(input_list)
+new_list = [number for number in no_repetitions(input_list)]
+print(new_list)
